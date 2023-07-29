6d84ba84d109339c6a919902671ee072efcfd504
Darryl-Shi <97209091+Darryl-Shi@users.noreply.github.com>
2023-07-29 15:16:31 +0800

    config

--- config/train_shakespeare_char.py
+++ config/train_shakespeare_char.py
@@ -10,8 +10,8 @@ log_interval = 10 # don't print too too often
 always_save_checkpoint = False
 
 wandb_log = False # override via command line if you like
-wandb_project = 'shakespeare-char'
-wandb_run_name = 'mini-gpt'
+wandb_project = 'quietattention'
+wandb_run_name = 'ghostmax'
 
 dataset = 'shakespeare_char'
 gradient_accumulation_steps = 1
